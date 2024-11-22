5808714f904a73d9f0dd5cd3a8e9a2ff5ec57bda
mehdizadeh-asma <mehdizadeh.asma@gmail.com>
2024-11-22 21:39:01 +0330

    Improve Not Found Page

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="text-center  bg-white vh-100 d-flex align-items-center justify-content-center">
       <Image alt="404" src="/404.jpg" width={380} height={253}></Image>
     </div>
   );
