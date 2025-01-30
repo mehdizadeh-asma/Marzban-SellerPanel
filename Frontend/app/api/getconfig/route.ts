a60fbe4ad04dfc9ae12457738f5056005e1bd002
mehdizadeh-asma <mehdizadeh.asma@gmail.com>
2025-01-30 21:36:49 +0330

    Fix Bug : Just the necessary data in the frontend

--- Frontend/app/api/getconfig/route.ts
+++ Frontend/app/api/getconfig/route.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import { promises as fs } from "fs";
 import { encrypt } from "@/utils/Crypto";
+import { JsonData } from "@/context/MyContext";
 
 export const dynamic = "force-dynamic";
 
@@ -9,7 +10,18 @@ export async function GET() {
     const filepath = path.join(process.cwd(), "data", "config.json");
     const fileContents = await fs.readFile(filepath, "utf8");
 
-    const encryptedContent = encrypt(fileContents);
+    const jd: JsonData = JSON.parse(fileContents);
+    const newjd = {
+      BACKEND_URL: jd.BACKEND_URL,
+      IGNORE_TRAFFIC_TO_REMOVE: jd.IGNORE_TRAFFIC_TO_REMOVE,
+      RENEW_FORCE_TO_PAID: jd.RENEW_FORCE_TO_PAID,
+      RENEW_FORCE_TO_LIMITED_AND_EXPIRED: jd.RENEW_FORCE_TO_LIMITED_AND_EXPIRED,
+      PAGE_TITLE: jd.PAGE_TITLE,
+      CHANNEL_NAME: jd.CHANNEL_NAME,
+    };
+    console.log(newjd);
+
+    const encryptedContent = encrypt(JSON.stringify(newjd));
 
     return new Response(encryptedContent);
   } catch (error) {
