d80164bfde1cfc1152b9a36ef49be68f9678ed12
mehdizadeh-asma <mehdizadeh.asma@gmail.com>
2025-09-12 04:49:47 +0330

    Add ESLint configuration and update TypeScript settings: Introduce eslint.config.js, remove module type from package.json, and adjust tsconfig.json includes.

--- Frontend/eslint.config.js
+++ Frontend/eslint.config.js
@@ -1,11 +1,11 @@
 // eslint.config.js
-import tsParser from "@typescript-eslint/parser";
+import js from "@eslint/js";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 import importPlugin from "eslint-plugin-import";
+import prettierPlugin from "eslint-plugin-prettier";
 import reactPlugin from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
-import prettierPlugin from "eslint-plugin-prettier";
-import js from "@eslint/js";
 import globals from "globals";
 
 export default [
